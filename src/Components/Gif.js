f58b807ae13a88e8374ffd6e1555574a24f908f8
mmrahmad <mmrahmad91@gmail.com>
2021-07-14 13:13:41 +0600

    Image place holder added

--- src/Components/Gif.js
+++ src/Components/Gif.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ImagePlaceHolder from './ImagePlaceHolder'
 
-const Gif = ({url}) =>{
-  return(
-    <li className="gif-wrap">
-      <img src={url} alt=""/>
-    </li>
-  ); 
+export default class Gif extends Component{
+  state={
+    loaded: false
+  }
+  onLoadHandeler = () => {
+    this.setState({
+      loaded: true
+    })
+  }
+  render(){
+    const {url} = this.props;
+    const {loaded} = this.state;
+    return(
+      <li className="gif-wrap">
+        {!loaded && <ImagePlaceHolder /> }
+        <img src={url} onLoad={this.onLoadHandeler} alt=""/>
+      </li>
+    )
+  }
 }
-export default Gif;
---
+++ src/Components/ImagePlaceHolder.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Loading from '../assets/Loading.png';
+
+export default function ImagePlaceHolder() {
+    return (
+        <img src={Loading} />
+    )
+}
+
